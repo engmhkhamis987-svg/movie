b5202a06a97470462df3ffa2c369173497bbace9
Mohammed Hassan <Mohammed.Hassan@SCA.local>
2026-06-04 11:53:24 +0300

    First

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/core/services/services_locator.dart';
-import 'package:movie_app/movies/presentation/screens/movie_screen.dart';
+import 'package:movie_app/movies/presentation/screens/movies_screen.dart';
 
 void main() {
   ServicesLocator().init();
@@ -12,11 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Movie App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MovieScreen(),
+      home: const MoviesScreen(),
     );
   }
 }
